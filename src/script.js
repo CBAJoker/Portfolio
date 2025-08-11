29b0199c03f8e55f41e4f2cf7bb94d01cc06032e
CBAJoker <cadetmasoom@gmail.com>
2025-08-11 20:55:41 +0500

    fixes bugs and maintains code quality 4.0.1

---
+++ src/script.js
@@ -0,0 +1,10 @@
+ tailwind.config = {
+            theme: {
+                extend: {
+                    colors: {
+                        'soft-blue': '#e0f2fe',
+                        'medium-blue': '#3b82f6',
+                    }
+                }
+            }
+        }
